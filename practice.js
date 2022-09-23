19e5ab63af7c706b6c29a9e92255bb70971c6598
Amisha7165 <110964187+Amisha7165@users.noreply.github.com>
2022-09-23 10:37:29 +0530

    Create practice.js

---
+++ practice.js
@@ -0,0 +1,11 @@
+function multiplyBy(){
+    num1 = document.getElementById("first number").value;
+    num2 = document.getElementById("second number").value;
+    document.getElementById("result").innerHTML= num1*num2;
+}
+function divideBy(){
+    num1 = document.getElementById("first number").value;
+    num2 = document.getElementById("second number").value;
+    document.getElementById("result").innerHTML = num1/num2;
+    
+}
